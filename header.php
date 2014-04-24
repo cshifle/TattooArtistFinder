d5be0479d1a258a43ac43dc9e99fba4ce708b7b1
cshifle <cshiffle1991@gmail.com>
2014-04-24 09:13:20 -0400

    touched up header and added them to all pages

--- header.php
+++ header.php
@@ -1,10 +1,6 @@
-<html>
-<head>
-
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-
-<style type="text/css">
-
+ <html>
+ <head>
+ <style type="text/css">
     * {margin: 0; padding: 0;}
 
     body {
@@ -26,40 +22,64 @@
 
     nav li{
     display: inline-block;
-    padding: 5px 30px 30px 0;
-    }
+    padding: 40px 30px 40px 0;
+
+/*    color: #FFFFFF;
+    text-decoration: none;
+    text-align: center;
+    background-color #000000;
+    border-left-width:thick;
+    border-left-style:solid;
+    border-left-color:99FF00*/
+        }
 
     nav li:nth-child(3) {
-    padding-right: 0px;
+    padding-right: 80px;
     }
 
     nav li:nth-child(4) {
-    padding-left: 0px;
+    padding-left: 80px;
     }
-    nav li:nth-child(5) {
-    padding-left: 0px;
-    }    
+    
+    a:link{
+    color:black;
+    font-family: Impact, Charcoal, sans-serif;
+    font-size: 24;
+    text-decoration: none;
+  
+    }
+    a:visited{
+    color:black;
+    }
 
-</style>
+    .logo {
+    background: url(images/logo/logo_thumb.png);
+    content: url(home.php);
+    background-size: 110px 120px;
+    width: 110px;
+    height: 120px;
+    position: absolute;
+    top: 5px;
+    left: 400px; 
+    }
+    </style>
 </head>
-<body>
-
-
-<section>
-<header>
-  <nav>
-   <ul>
-    <li><a href="clientInfo.php">Client</a></li>
-    <li><a href="artistInfo.php">Artists</a></li>
-    <li><a href="shopInfo.php">Shops</a></li>
-    <li><a href="home.php"><img src="images/logo/logo_small.png"></a></li>
-    <li><a href="genres.php">Genres</a></li>
-    <li><a href="advice.php">Advice</a></li>
-    <li><a href="aftercare.php">Aftercare</a></li>
-   </ul>
-  </nav>
-
+<body>    
+    <section>
+		<header>
+ 		 <nav>
+  		   <ul>
+ 		     <li><a href="clientInfo.php">Clients</a></li>
+  		     <li><a href="artistInfo.php">Artists</a></li>
+ 		     <li><a href="shopInfo.php">Shops</a></li>
+ 		     <li><a href="genres.php">Genres</a></li>
+ 		     <li><a href="advice.php">Advice</a></li>
+ 		     <li><a href="aftercare.php">Aftercare</a></li>
+ 		  </ul>
+         </nav>
+  <a href="home.php"><div class="logo"></div></a>
 </header>
 </section>
+
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -1,1 +1,65 @@
-This is a test
+ <html>
+ <head>
+ <style type="text/css">
+    * {margin: 0; padding: 0;}
+
+    body {
+    background: #f5f5f5;
+    font-family: arial;
+    }
+
+    section {
+    background: #fff;
+    box-shadow: 0px 2px 2px #ebebeb;
+    }
+
+    header {
+    width: 960px;
+    margin: 0 auto;
+    text-align: center;
+    position: relative;
+    }
+
+    nav li{
+    display: inline-block;
+    padding: 40px 30px 40px 0;
+    }
+
+    nav li:nth-child(3) {
+    padding-right: 80px;
+    }
+
+    nav li:nth-child(4) {
+    padding-left: 80px;
+    }
+
+    .logo {
+    background: url(images/logo/logo_thumb.png);
+    background-size: 110px 120px;
+    width: 110px;
+    height: 120px;
+    position: absolute;
+    top: 20px;
+    left: 400px; 
+    }
+    </style>
+</head>
+<body>    
+    <section>
+<header>
+  <nav>
+   <ul>
+    <li><a href="clientInfo.php">Client</a></li>
+    <li><a href="artistInfo.php">Artists</a></li>
+    <li><a href="shopInfo.php">Shops</a></li>
+    <li><a href="genres.php">Genres</a></li>
+    <li><a href="advice.php">Advice</a></li>
+    <li><a href="aftercare.php">Aftercare</a></li>
+   </ul>
+  </nav>
+  <div class="logo"></div>
+</header>
+</section>
+
+</body>
+</html>
